e0c7b595239add69b99e566c7454e539f11303c1
Ross Barnowski <rossbar@caltech.edu>
2026-05-01 13:53:47 -0700

    Get rid of function in profiling script
    Dump everything to main namespace.

--- profile_predict.py
+++ profile_predict.py
@@ -12,25 +12,20 @@ z = zarr.open(zname, mode="r")
 k = "HBM222_WQKC_382"
 model_path = Path.home() / "saved_model_best_dict_849700.pth"
 
-def profile_main(k, model_path):
-    ds = z[k]
-    img = ds["image"][:]
-    chnames = ds["image"].attrs["channels"]
-    nuc, mem = ds.attrs["nuclear_channel"], ds.attrs["membrane_channel"]
-    mpp = ds["image"].attrs["mpp"]
+ds = z[k]
+img = ds["image"][:]
+chnames = ds["image"].attrs["channels"]
+nuc, mem = ds.attrs["nuclear_channel"], ds.attrs["membrane_channel"]
+mpp = ds["image"].attrs["mpp"]
 
-    im = np.stack(
-        [img[chnames.index(nuc)], img[chnames.index(mem)]],
-        axis=0,
-    )
-    app = Mesmer(model_path=model_path, device="cuda:1")
+im = np.stack(
+    [img[chnames.index(nuc)], img[chnames.index(mem)]],
+    axis=0,
+)
+app = Mesmer(model_path=model_path, device="cuda:1")
 
-    tic = time.time()
-    mask = app.predict(im[np.newaxis, ...], image_mpp=mpp, batch_size=4, compartment="whole-cell")[0].squeeze()
-    toc = time.time()
+tic = time.time()
+mask = app.predict(im[np.newaxis, ...], image_mpp=mpp, batch_size=4, compartment="whole-cell")[0].squeeze()
+toc = time.time()
 
-    print(toc - tic)
-    return mask
-
-if __name__ == "__main__":
-    profile_main(k, model_path)
+print(toc - tic)
